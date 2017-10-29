e62585968df402f955d85b49694380617bfe77ae
rAnYKM <jcjy21@gmail.com>
2017-10-28 21:19:46 -0400

    DBSCAN cluster

--- src/ran_attack.py
+++ src/ran_attack.py
@@ -3,32 +3,43 @@
 import sys
 
 from process import folder_setup, data_process
-from emb import ul_graph_build, para_ul_random_walk, emb_train
+from emb import ul_graph_build, para_ul_random_walk, emb_train, para_ul_random_batch
 from predict import feature_construct, unsuper_friends_predict
 from ran_figure import load_test_scores
 
-city = "Brightkite"# ny la london Gowalla Brightkite
-cicnt = 20
 
-folder_setup(city)
-checkin, friends = data_process(city, cicnt)
+def single_run(city, cicnt=20)
+    # city = "Brightkite"# ny la london Gowalla Brightkite
+    # cicnt = 20
 
-# ul_graph, lu_graph = ul_graph_build(checkin, 'locid')
+    folder_setup(city)
+    checkin, friends = data_process(city, cicnt)
 
-model_name = str(cicnt) + '_locid'
-print(model_name)
+    ul_graph, lu_graph = ul_graph_build(checkin, 'locid')
 
-walk_len, walk_times = 100, 20 # maximal 100 walk_len, 20 walk_times
+    model_name = str(cicnt) + '_locid'
+    print(model_name)
 
-print('walking')
-#para_ul_random_walk(city, model_name, checkin.uid.unique(), ul_graph, lu_graph,
-#                    walk_len, walk_times)
-print('walk done')
+    walk_len, walk_times = 100, 20 # maximal 100 walk_len, 20 walk_times
 
-print('emb training')
-#emb_train(city, model_name)
-print('emb training done')
+    print('walking')
+    para_ul_random_batch(city, model_name, checkin.uid.unique(), ul_graph, lu_graph,
+                        walk_len, walk_times)
+    print('walk done')
 
-#feature_construct(city, model_name, friends)
-#unsuper_friends_predict(city, model_name)
-load_test_scores(city, model_name)
+    print('emb training')
+    emb_train(city, model_name)
+    print('emb training done')
+
+    feature_construct(city, model_name, friends)
+    unsuper_friends_predict(city, model_name)
+    # load_test_scores(city, model_name)
+
+def multi_eps(city, region, cicnt=20):
+    for eps in [5, 10, 15, 20, 25, 30]:
+        single_run(city + '_' + region + '_' + str(eps), cicnt)
+
+
+if __name__ == '__main__':
+    multi_eps('Gowalla', 'na', 20)
+    
--- src/snap_dataset.py
+++ src/snap_dataset.py
@@ -14,6 +14,10 @@ CHECKIN_SUFFIX = "_totalCheckins.txt"
 #CONSTANT
 FZERO = 0.0000001
 
+#REGION
+REGIONS = {'na': [(9, -170), (71, -46)],
+           'usw': [(32, -123), (49, -104)]}
+
 
 def __checkin_process(line):
     items = line.strip().split('\t')
@@ -24,7 +28,7 @@ def __checkin_process(line):
         # print(items)
         return None
     return {'user': items[0],
-            'time': time.mktime(time.strptime(items[1], "%Y-%m-%dT%H:%M:%SZ")),
+            #'time': time.mktime(time.strptime(items[1], "%Y-%m-%dT%H:%M:%SZ")),
             'latitude': float(items[2]),
             'longitude': float(items[3]),
             'location': items[4]}
@@ -81,6 +85,93 @@ def gen_w2f_dataset(name, active_threshold, granularity):
     print('done')
 
 
+def region_dataset(name, active_threshold, region):
+    edges, checkins = load_snap_dataset(name)
+
+    min_lat, min_lon = REGIONS[region][0]
+    max_lat, max_lon = REGIONS[region][1]
+    checkins = checkins[(checkins.latitude >= min_lat) &
+                        (checkins.latitude <= max_lat) &
+                        (checkins.longitude >= min_lon) &
+                        (checkins.longitude <= max_lon)]
+    checkins = checkins.assign(counts=checkins.groupby(['user'])['user'] \
+                               .transform(np.size))
+    fil = checkins[checkins.counts >= active_threshold]
+    fil = fil[['user', 'latitude', 'longitude']]
+    user_list = pd.unique(fil['user'])
+    print(len(user_list), fil.shape)
+    g = nx.DiGraph()
+    g.add_edges_from(edges)
+    sub = g.subgraph(user_list)
+    new_edges = [{'u1': e[0], 'u2': e[1]} for e in sub.edges()]
+
+    edge_table = pd.DataFrame(new_edges)
+    fil.to_csv(os.path.join(SNAP_DATASET_DIR,
+                            '_'.join([name, region, str(active_threshold)])
+                            + CHECKIN_SUFFIX)
+               )
+    edge_table.to_csv(os.path.join(SNAP_DATASET_DIR,
+                                   '_'.join([name,
+                                             region,
+                                             str(active_threshold)])
+                                + EDGE_SUFFIX),
+                      index=False)
+    print('done')
+
+
+def gen_region_w2f_dataset(name, active_threshold, region, granularity, clustering=None):
+    fil = pd.read_csv(os.path.join(SNAP_DATASET_DIR,
+                                   '_'.join([name,
+                                             region,
+                                             str(active_threshold)])
+                                   + CHECKIN_SUFFIX),
+                      index_col=0)
+    edge = pd.read_csv(os.path.join(SNAP_DATASET_DIR,
+                                    '_'.join([name,
+                                              region,
+                                              str(active_threshold)])
+                                    + EDGE_SUFFIX))
+    # apply granularity
+    enlarge = 1 / granularity
+    fil.loc[:, 'latitude'] = fil.apply(lambda x: int(x['latitude'] * enlarge),
+                                        axis=1)
+    fil.loc[:, 'longitude'] = fil.apply(lambda x: int(x['longitude'] * enlarge),
+                                        axis=1)
+    locs = set(zip(fil.latitude, fil.longitude))
+    # location num
+    print(len(locs))
+
+    par = 0
+
+    if clustering is None:
+        aux = {loc: i for i, loc in enumerate(locs)}
+        fil.loc[:, 'locid'] = fil.apply(lambda x: aux[(x['latitude'],
+                                                       x['longitude'])],
+                                      axis=1)
+
+    elif clustering['model'] == 'DBSCAN':
+        cluster = DBSCAN(clustering['eps'], clustering['min_samples'],
+                         n_jobs=clustering['n_jobs']).fit(list(locs))
+        aux = {loc: cluster.labels_[i] for i, loc in enumerate(locs)}
+        print(len(set(cluster.labels_)), len(pd.unique(fil['user'])))
+        fil.loc[:, 'locid'] = fil.apply(lambda x: aux[(x['latitude'],
+                                                       x['longitude'])],
+                                      axis=1)
+        par = clustering['eps']
+
+    fil = fil[['user', 'locid']]
+    fil.rename(columns={'user': 'uid'}, inplace=True)
+    fil.to_csv('dataset/%s_%s_%d_%d.checkin' % (name,
+                                                region,
+                                                par,
+                                                active_threshold))
+    edge.to_csv('dataset/%s_%s_%d_%d.friends' % (name,
+                                                 region,
+                                                 par,
+                                                 active_threshold))
+
+
+
 def remap_locid(checkin_file):
     table = pd.read_csv('dataset/%s.checkin' % checkin_file,
                         index_col=0)
@@ -122,6 +213,7 @@ def gen_Kmeans_dataset(name, active_threshold, granularity, k):
     print('here')
     # fil['loc'] = DBSCAN(eps=1, min_samples=100, n_jobs=-1).fit_predict(locs)
     fil['loc'] = KMeans(20, n_jobs=-1).fit_predict(locs)
+
     # only keep location id, user
     new_table = fil[['user', 'loc']]
     print(len(pd.unique(new_table['loc'])))
@@ -146,5 +238,12 @@ def gen_Kmeans_dataset(name, active_threshold, granularity, k):
 
 # gen_w2f_dataset(SNAP_DATASET_NAMES[1], 20, 0.01)
 # remap_locid("Gowalla_20")
-gen_Kmeans_dataset(SNAP_DATASET_NAMES[0], 20, 0.001, 10000)
-dataset_summary(SNAP_DATASET_NAMES[0] + '_10000M')
+# gen_Kmeans_dataset(SNAP_DATASET_NAMES[0], 20, 0.001, 10000)
+# dataset_summary(SNAP_DATASET_NAMES[0] + '_10000M')
+# region_dataset(SNAP_DATASET_NAMES[1], 20, 'na')
+for i in [5, 10, 15, 25, 30]:
+    gen_region_w2f_dataset(SNAP_DATASET_NAMES[1], 20, 'na', 0.001,
+                            {'model': 'DBSCAN',
+                            'eps': i,
+                            'min_samples': 1,
+                            'n_jobs': -2})
